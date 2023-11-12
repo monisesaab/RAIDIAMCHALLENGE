383f8fc9c2d47b2ab8ae84576fccefaa374b59c5
Monise Saab <monisesaab@email.com>
2023-11-12 19:51:10 -0300

    Adding Booking pending scenario, Booking confirmed scenario and Booking canceled scenario

--- FrontEnd-test/cypress/support/commands.js
+++ FrontEnd-test/cypress/support/commands.js
@@ -15,6 +15,19 @@ Cypress.Commands.add('login', (email, password) => {
   cy.clickGet('.header_menu > :nth-child(1) > .nav-link')
 })
 
+
+Cypress.Commands.add('accessMyBookings', (email, password) => { 
+  cy.clickLink('Account')
+  cy.clickLink('Bookings')
+})
+
+
+Cypress.Commands.add('searchMyBooking',(bookingNumber) => {
+  cy.get('#DataTables_Table_0_filter > label > .form-control').type(bookingNumber)
+})
+
+
+
 Cypress.Commands.add('searchFligths',(from, to, date, travellersAdult,travellersChilds,travellersInfants) => {
   cy.get('#onereturn > :nth-child(1) > .input-items > .form-floating > .select2 > .selection > .select2-selection > .select2-selection__arrow > b').click()
   cy.clickButton(from)
@@ -58,13 +71,15 @@ Cypress.Commands.add('confirmPassegersDetails',(firstName,lastName,passaportNumb
 })
 
 Cypress.Commands.add('checkBookingPaymentStatus',(status) => {
-  cy.get('.col-sm-8 > :nth-child(1) > :nth-child(1)').should('be.visible')  
   cy.get('.col-sm-8 > :nth-child(1) > :nth-child(1)').should('contain', status);
 })
 
 Cypress.Commands.add('checkBookingStatus',(status) => {
-  cy.get('.col-sm-8 > :nth-child(1) > :nth-child(2)').should('be.visible')  
   cy.get('.col-sm-8 > :nth-child(1) > :nth-child(2)').should('contain', status);
+})
+
+Cypress.Commands.add('checkBookingStatusOnList',(status) => {
+  cy.get('.odd > :nth-child(3)').should('contain', status);
 })
 
 Cypress.Commands.add('checkTheFlightSearched',(from,to,date) => {
