79808340cef163a20e18e367162731ea85d951b3
Monise Saab <monisesaab@email.com>
2023-11-13 20:55:44 -0300

    Starting Backend-Test

---
+++ BackEnd-test/tests.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+const { expect } = require('chai');
+
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+const apiKey = 'apiKey';
+
+describe('Current and forecasts weather data', () => {
+
+
+});
